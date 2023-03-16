e6505c7d64a7f8b554559f9339d5a5e15a2dc827
+ <+>
2023-03-16 03:45:40 +0300

    Completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction']}
+
+    validate :title_is_clickbaity
+
+    private
+
+    def title_is_clickbaity
+        if title.present? && !title.include?("Won't Believe") && !title.include?("Secret") && !title.include?("Top") && !title.include?("Guess")
+          errors.add(:title, "must be sufficiently clickbait-y")
+        end
+    end
+
 end
